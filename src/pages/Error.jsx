61d3280e195efd4e896b344c4dcc25423cbdcd12
sai shravan <saishravan384@gmail.com>
2024-12-13 14:40:52 +0530

    feat: Add basic routing

---
+++ src/pages/Error.jsx
@@ -0,0 +1,7 @@
+export default function Error() {
+  return (
+    <div className='error'>
+      <p>Error</p>
+    </div>
+  )
+}
---
+++ src/pages/Service.jsx
@@ -0,0 +1,7 @@
+export default function Service() {
+  return (
+    <div className='service'>
+      <p>Service</p>
+    </div>
+  )
+}
