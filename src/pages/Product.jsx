2f2cafd41f7ef1dc9c51752e673ec383c499dc68
sai shravan <saishravan384@gmail.com>
2024-12-14 11:03:42 +0530

    feat: Add Product page with styles

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,3 +1,4 @@
+import '../styles/Product.css'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
@@ -6,7 +7,34 @@ export default function Product() {
     <>
       <Navbar />
       <div className='product'>
-        <p>Product</p>
+        <div className='item'>
+          <img src='/product1.jpeg' alt='product 1' />
+          <p>
+            Reverse Osmosis (RO) + Ultra Filtration (UF)+ Minerals (Ca, Mg,
+            Copper)
+          </p>
+        </div>
+
+        <div className='item'>
+          <img src='/product2.jpeg' alt='product 2' />
+          <p>Only Reverse Osmosis (RO)</p>
+        </div>
+
+        <div className='item'>
+          <img src='/product3.jpeg' alt='product 3' />
+          <p>
+            Green Purifier: Ultraviolet (UV) + Ultra Filtration (UF) + Minerals
+            (Ca, Mg, Copper)
+          </p>
+        </div>
+
+        <div className='item'>
+          <img src='/product4.jpeg' alt='product 4' />
+          <p>
+            Reverse Osmosis (RO) + Ultraviolet (UV)+ Ultra Filtration (UF)+
+            Minerals (Ca, Mg, Copper)
+          </p>
+        </div>
       </div>
       <Footer />
     </>
